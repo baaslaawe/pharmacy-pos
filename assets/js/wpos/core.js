b42aa141e75ecb140b46c973af5aa027d9fc718e
JOE NYUGOH NGIGI <nyugoh@gmail.com>
2018-04-18 14:36:12 +0300

    Master rebranding to biashara pos

--- assets/js/wpos/core.js
+++ assets/js/wpos/core.js
@@ -615,7 +615,7 @@ function WPOS() {
         swal({
             type: 'error',
             title: 'Oops...',
-            text: 'Your internet connection is not active and Pharmacy POS has started in offline mode.\nSome features are not available in offline mode but you can always make sales and alter transactions that are locally available. \nWhen a connection becomes available the POS will process your transactions on the server.'
+            text: 'Your internet connection is not active and Biashara Retail POS has started in offline mode.\nSome features are not available in offline mode but you can always make sales and alter transactions that are locally available. \nWhen a connection becomes available the POS will process your transactions on the server.'
           });
           
           
@@ -625,7 +625,7 @@ function WPOS() {
     function initDataSuccess(loginloader){
         if (loginloader){
             setLoadingBar(100, "Massaging the data...");
-            $("title").text("Pharmacy+ POS");
+            $("title").text("Biashara Retail POS");
             WPOS.initPlugins();
             populateDeviceInfo();
             setTimeout(hideLogin, 500);
