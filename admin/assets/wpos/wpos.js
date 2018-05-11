22a3d414547e1e5f1db4bb73fa88044ce59a3f47
nyugoh <nyugoh@gmail.com>
2018-05-11 14:51:50 +0300

    Hard coded listening port for realtime section to port 90

--- admin/assets/wpos/wpos.js
+++ admin/assets/wpos/wpos.js
@@ -668,7 +668,7 @@ function WPOSAdmin(){
         if (socket === null){
             var proxy = this.getConfigTable().general.feedserver_proxy;
             var port = this.getConfigTable().general.feedserver_port;
-            var socketPath = window.location.protocol+'//'+window.location.hostname+(proxy==false ? ':'+port : '');
+            var socketPath = 'http://localhost:90'+(proxy==false ? ':'+port : '');
             socket = io.connect(socketPath);
             socketon = true;
             socket.on('connect_error', socketError);
