29aac040ca2262a5082dd5365baaccf916fbca3f
nyugoh <nyugoh@gmail.com>
2018-05-16 10:29:32 +0300

    Fixed [bugfix](https://github.com/nyugoh/pharmacy-pos/projects/3#card-9731460)

--- assets/js/wpos/transactions.js
+++ assets/js/wpos/transactions.js
@@ -368,7 +368,7 @@ function WPOSTransactions() {
         $("#transsubtotal").text(WPOS.util.currencyFormat(record.subtotal));
         populateTaxinfo(record);
         if (record.discount>0){
-            $("#transdiscount").text(record.discount+"% ("+WPOS.util.currencyFormat((parseFloat(record.total)-Math.abs(parseFloat(record.subtotal)+parseFloat(record.tax))).toFixed(2))+')');
+            $("#transdiscount").text(WPOS.util.currencyFormat(record.discount));
             $("#transdisdiv").show();
         } else {
             $("#transdisdiv").hide();
