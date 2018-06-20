d495d94c0b19b4fb1caa271c3716c734e24cede4
nyugoh <nyugoh@gmail.com>
2018-06-20 15:01:30 +0300

    Added isDaa into schema and on stored items

--- library/wpos/models/db/StoredItemsModel.php
+++ library/wpos/models/db/StoredItemsModel.php
@@ -29,7 +29,7 @@ class StoredItemsModel extends DbConfig
     /**
      * @var array available columns
      */
-    protected $_columns = ['id' ,'data', 'categoryid', 'name', 'taxid', 'reorderPoint'];
+    protected $_columns = ['id' ,'data', 'categoryid', 'name', 'taxid', 'stockType', 'isDaa', 'reorderPoint'];
 
     /**
      * Init DB
@@ -45,10 +45,10 @@ class StoredItemsModel extends DbConfig
      */
     public function create($data)
     {
-        $dataObj = json_encode(['name'=>$data->name, 'description'=>$data->description, 'categoryid'=>$data->categoryid, 'taxid'=>$data->taxid, 'reorderPoint'=>$data->reorderPoint, 'stockType'=>$data->stockType]);
+        $dataObj = json_encode(['name'=>$data->name, 'description'=>$data->description, 'categoryid'=>$data->categoryid, 'taxid'=>$data->taxid, 'reorderPoint'=>$data->reorderPoint, 'stockType'=>$data->stockType, 'isDaa'=>$data->isDaa]);
 
-        $sql          = "INSERT INTO stored_items (`data`, `categoryid`, `name`, `description`, `taxid`, `reorderPoint`, `stockType`) VALUES (:data, :categoryid, :name, :description, :taxid, :reorderPoint, :stockType);";
-        $placeholders = [":data"=>$dataObj, ":categoryid"=>$data->categoryid, ":name"=>$data->name, ":description"=>$data->description, ":taxid"=>$data->taxid, ":reorderPoint"=>$data->reorderPoint, ":stockType"=>$data->stockType];
+        $sql          = "INSERT INTO stored_items (`data`, `categoryid`, `name`, `description`, `taxid`, `reorderPoint`, `stockType`, `isDaa`) VALUES (:data, :categoryid, :name, :description, :taxid, :reorderPoint, :stockType, :isDaa);";
+        $placeholders = [":data"=>$dataObj, ":categoryid"=>$data->categoryid, ":name"=>$data->name, ":description"=>$data->description, ":taxid"=>$data->taxid, ":reorderPoint"=>$data->reorderPoint, ":stockType"=>$data->stockType, ":isDaa"=>$data->isDaa];
 
         return $this->insert($sql, $placeholders);
     }
@@ -122,8 +122,8 @@ class StoredItemsModel extends DbConfig
      */
     public function edit($id, $data){
 
-        $sql = "UPDATE stored_items SET data= :data, categoryid= :categoryid, name= :name, taxid= :taxid, reorderPoint= :reorderPoint, stockType= :stockType WHERE id= :id;";
-        $placeholders = [":id"=>$id, ":data"=>json_encode($data), ":categoryid"=>$data->categoryid, ":name"=>$data->name, ":taxid"=>$data->taxid, ":reorderPoint"=>$data->reorderPoint, ":stockType"=>$data->stockType];
+        $sql = "UPDATE stored_items SET data= :data, categoryid= :categoryid, name= :name, taxid= :taxid, reorderPoint= :reorderPoint, stockType= :stockType, isDaa= :isDaa WHERE id= :id;";
+        $placeholders = [":id"=>$id, ":data"=>json_encode($data), ":categoryid"=>$data->categoryid, ":name"=>$data->name, ":taxid"=>$data->taxid, ":reorderPoint"=>$data->reorderPoint, ":stockType"=>$data->stockType, ":isDaa"=>$data->isDaa];
 
         return $this->update($sql, $placeholders);
     }
